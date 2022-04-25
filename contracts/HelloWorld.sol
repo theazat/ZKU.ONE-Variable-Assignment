321fd141fd370d08e13f40f0639603d05f3836b0
Azat Sayan <sayanazat@gmail.com>
2022-04-25 14:36:59 +0300

    Update HelloWorld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,16 +4,22 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract HelloWorld {
 
-    //declaring an unsigned integer
+    // declaring an unsigned integer
     uint myNumber;
 
-    // to store our unsigned integer
+    /**
+     * @dev Store value in variable
+     * @param _myNumber value to store
+     */
     function storeNumber(uint _myNumber) public {
-        // //Setting the value of our variable myNumber with new value.
+        // Setting the value of our variable myNumber with new value.
         myNumber = _myNumber;
     }
 
-    // to retrieve that our declared uunsigned integer
+    /**
+     * @dev Return value 
+     * @return value of 'myNumber'
+     */
     function retrieveNumber()  public view returns(uint){
         return myNumber;
     }
